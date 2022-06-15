3f780c3c4b6910a78725aaef2a482872bf7f67ca
Hide-andSeek <jade.arinal.banares@gmail.com>
2022-06-15 10:03:13 +0800

    Adding JS

---
+++ javascript/functionality.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.7)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+    document.body.style.backgroundColor = "white";
+  }
